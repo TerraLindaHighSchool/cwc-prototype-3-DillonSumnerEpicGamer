2e441c5412e9090ffdbb8c3fc476d68cfa40f656
Dillon Sumner <56370697+DillonSumnerEpicGamer@users.noreply.github.com>
2020-12-03 14:51:11 -0800

    Unit 3 Lesson 3.1 Complete

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody plrRB;
+    private bool isGrounded = true;
+
+    public float jumpForce = 10.0f;
+    public float gravityModifier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        plrRB = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            plrRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obsaclePrefab;
+
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2.0f;
+    private float repeatingRate = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatingRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacle()
+    {
+        Instantiate(obsaclePrefab, spawnPos, obsaclePrefab.transform.rotation);
+    }
+}
